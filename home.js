59649b0d87c77705d8d5d7edf7358afd6745bf3a
nwarner31 <natedawg3124@gmail.com>
2023-11-15 17:25:55 -0800

    initial commit

---
+++ home.js
@@ -0,0 +1,8 @@
+const form = document.getElementById("name-form");
+const player1 = document.getElementById()
+function submitNames(event) {
+    event.preventDefault();
+
+}
+
+form.addEventListener("submit", submitNames);
